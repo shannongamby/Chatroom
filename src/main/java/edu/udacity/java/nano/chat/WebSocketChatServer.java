1bd39b752342914a7ed30123c0f4869702a62b40
Shannon Gamby <shannongamby@hotmail.com>
2019-09-14 14:35:05 +0100

    update functions to use static type

--- src/main/java/edu/udacity/java/nano/chat/WebSocketChatServer.java
+++ src/main/java/edu/udacity/java/nano/chat/WebSocketChatServer.java
@@ -8,6 +8,8 @@ import java.io.IOException;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+import static edu.udacity.java.nano.chat.Message.*;
+
 @Component
 @ServerEndpoint("/chat")
 public class WebSocketChatServer {
@@ -29,19 +31,19 @@ public class WebSocketChatServer {
         if(onlineSessions.containsKey(session.getId())) { return; }
 
         onlineSessions.put(session.getId(), session);
-        sendMessageToAll(Message.jsonConverter("ENTER", "", "", onlineSessions.size()));
+        sendMessageToAll(Message.jsonConverter(ENTER, "", "", onlineSessions.size()));
     }
 
     @OnMessage
     public void onMessage(Session session, String jsonStr) {
         Message msg = JSON.parseObject(jsonStr, Message.class);
-        sendMessageToAll(Message.jsonConverter("SPEAK", msg.getUsername(), msg.getMessage(), onlineSessions.size()));
+        sendMessageToAll(Message.jsonConverter(SPEAK, msg.getUsername(), msg.getMessage(), onlineSessions.size()));
     }
 
     @OnClose
     public void onClose(Session session) {
         onlineSessions.remove(session.getId());
-        sendMessageToAll(Message.jsonConverter("QUIT", "", "", onlineSessions.size()));
+        sendMessageToAll(Message.jsonConverter(QUIT, "", "", onlineSessions.size()));
     }
 
     @OnError
